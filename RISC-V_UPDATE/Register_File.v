228e63f133db018dfdfb7a072c0b8eb6da4a8144
Harshbir-Singh <harshbir1279@gmail.com>
2026-03-22 01:29:18 +0530

    Add files via upload

---
+++ RISC-V_UPDATE/Register_File.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+module Register_File(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
+
+    input clk,rst,WE3;
+    input [4:0]A1,A2,A3;
+    input [31:0]WD3;
+    output [31:0]RD1,RD2;
+
+    reg [31:0] Register [31:0];
+    integer i;
+    always @ (posedge clk)
+    begin
+        if(WE3 & (A3 != 5'h00))
+            Register[A3] <= WD3;
+    end
+
+    assign RD1 = (~rst) ? 32'd0 :
+                 (WE3 && A3==A1 && A3!=0) ? WD3:
+                  Register[A1];
+
+    assign RD2 = (~rst)? 32'd0 :
+                 (WE3 && A3==A2 && A3!=0) ? WD3:
+                  Register[A2];
+    initial begin
+        for(i = 0; i < 32; i = i+1)
+            Register[i] = 32'h00000000;
+    end
+    
+endmodule
